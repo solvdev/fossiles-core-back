21ccab9a25c8a267bb35826b27458f86235a66cf
Eduardo Ramirez <eduardorh1312@gmail.com>
2026-07-17 08:37:14 -0600

    correcciones conteo

--- src/main/java/com/fossiles/fossilescorebackend/application/service/KioskPosService.java
+++ src/main/java/com/fossiles/fossilescorebackend/application/service/KioskPosService.java
@@ -92,6 +92,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
